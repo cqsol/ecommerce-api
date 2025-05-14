62292798d17d494590d531c35002de6d54e0349e
Puripat <puripat.sriputthimeth@gmail.com>
2025-05-14 15:01:30 +0700

    enable core

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  
+  // Enable CORS for localhost:3000
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
